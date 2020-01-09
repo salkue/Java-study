f3c96bb40c96560bb192bee911c36d36a9a6fd1f
salkue <59603029+salkue@users.noreply.github.com>
2020-01-09 07:35:20 +0300

    Update Person.java

--- Person/src/Person.java
+++ Person/src/Person.java
@@ -9,7 +9,7 @@ public class Person {
     }
 
     /**
-     * This method checks gender of persons. If genders are not equal - tries to marry.
+     * 111This method checks gender of persons. If genders are not equal - tries to marry.
      * If one of them has another spouse - execute divorce(sets spouse = null for husband and wife. Example: if both persons have spouses - then divorce will set 4 spouse to null) and then executes marry().
      *
      * @param person - new husband/wife for this person.
