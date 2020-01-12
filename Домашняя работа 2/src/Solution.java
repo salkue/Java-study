81fce788403694dd5167f573560ba899e9331551
Ivan Ayvazov <fess_forever@bk.ru>
2020-01-12 21:53:03 +0300

    update

---
+++ Домашняя работа 2/src/Solution.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    //Задание 1: Подсчитайте количество различных слов в файле.
+    public int uniqueWordCount(String str) {
+        Set <String> set=new HashSet<>();
+        String[] words=str.split("\\s")
+
+    }
+}
