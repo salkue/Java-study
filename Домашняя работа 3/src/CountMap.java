3100c76ac7daaacf17cd2adde7e0cdf19b530ed6
Ivan Ayvazov <fess_forever@bk.ru>
2020-01-18 19:34:14 +0300

    резалт

--- Домашняя работа 3/src/CountMap.java
+++ Домашняя работа 3/src/CountMap.java
@@ -5,20 +5,20 @@ public interface CountMap <T> {
     void add(T o);
 
     //Возвращает количество добавлений данного элемента
-    int getCount(Object o);
+    int getCount(T o);
 
     //Удаляет элемент из контейнера и возвращает количество его добавлений(до удаления)
-    int remove(Object o);
+    int remove(T o);
 
     //количество разных элементов
     int size();
 
     //Добавить все элементы из source в текущий контейнер, при совпадении ключей,     суммировать значения
-    void addAll(CountMap source);
+    void addAll(CountMap<T> source);
 
     //Вернуть java.util.Map. ключ - добавленный элемент, значение - количество его добавлений
-    Map toMap();
+    Map<T,Integer> toMap();
 
     //Тот же самый контракт как и toMap(), только всю информацию записать в destination
-    void toMap(Map destination);
+    void toMap(Map<T,Integer> destination);
 }
