ea4247248c709b9e2e2f9cc48dba87b2c5ecda2b
Ivan Ayvazov <fess_forever@bk.ru>
2020-05-10 12:49:55 +0300

    up

---
+++ MavenTest/src/main/java/NetworkUtils.java
@@ -0,0 +1,11 @@
+public class NetworkUtils {
+    public static void getConnection() {
+        // получаем соединение с сервером
+        try {
+            Thread.sleep(1500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return;
+    }
+}
